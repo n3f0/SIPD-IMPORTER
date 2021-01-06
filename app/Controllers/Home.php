bd66f99e7fe75cbcaa8179804e9cb33073e62780
n3f0 <nefo424@gmail.com>
2021-01-06 21:53:39 +0700

    Penambahan library Client SIPD

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,7 +1,12 @@
 <?php namespace App\Controllers;
 
+use App\Libraries\Client;
+
 class Home extends BaseController
 {
+	public function __construct(){
+		$this->client=new Client;
+	}
 	public function index()
 	{
 		return view('welcome_message');
---
+++ app/Libraries/Client.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Libraries;
+
+class Client{
+
+    public function post($url,$param,$cookie){
+
+    }
+
+    public function get($url,$cookie){
+
+    }
+
+    public function getCookie($data){
+
+    }
+}
