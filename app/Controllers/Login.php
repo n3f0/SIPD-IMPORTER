0472b62db22f08dfb37e0a4cf25854e099a66fa8
n3f0 <nefo424@gmail.com>
2021-01-06 23:55:30 +0700

    Penambahan session pada login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -1,9 +1,22 @@
 <?php
 namespace App\Controllers;
 
+use App\Libraries\Client;
+
 class login extends BaseController{
 
+    public function __construct(){
+        $this->client=new Client();
+        $this->session=session();
+    }
+
     public function index(){
+        $url="https://".SIPD.".sipd.kemendagri.go.id/daerah?kXR8NMlu7z81k3Aeh6cs4RSVsvvRbF3k7heGR1qLeXxp/IRMD@AeNPzWxJHwLOzc4nFclz9BCPCM5ABRsZDIEqu2JDMcfg5Q2@DAITGqXHkvlI94msbz1oVmlQ0whqm3";
+        $data=$this->client->get($url);
+        $token=$this->client->token($data['content']);
+        $cookie=$this->client->cookie($data['header']);
+        $this->session->set('token',$token);
+        $this->session->set('cookie',$cookie);
         return view('login');
     }
 }
--- app/Views/login.php
+++ app/Views/login.php
@@ -26,7 +26,7 @@
     </head>
     <body>
         <div id="login">
-            <h3 class="text-center text-white pt-5">SIPD Importing Tools</h3>
+            <h3 class="text-center text-white pt-5">SIPD Importing Tools <?=SIPD?></h3>
             <div class="container">
                 <div id="login-row" class="row justify-content-center align-items-center">
                     <div id="login-column" class="col-md-6">
@@ -35,11 +35,11 @@
                                 <h3 class="text-center text-info">Login</h3>
                                 <div class="form-group">
                                     <label for="username" class="text-info">Username:</label><br>
-                                    <input type="text" name="username" id="username" class="form-control">
+                                    <input type="text" name="username" id="username" class="form-control" placeholder="User SIPD">
                                 </div>
                                 <div class="form-group">
                                     <label for="password" class="text-info">Password:</label><br>
-                                    <input type="text" name="password" id="password" class="form-control">
+                                    <input type="text" name="password" id="password" class="form-control" placeholder="Password SIPD">
                                 </div>
                                 <div class="form-group">
                                     <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
@@ -55,4 +55,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://nusantaraprov.sipd.kemendagri.go.id/assets/js/crypto_base64.js"></script>
 </html>
