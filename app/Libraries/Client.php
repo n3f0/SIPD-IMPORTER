964bace4788b71e1e15e1b73cfe76cf478724b4e
n3f0 <nefo424@gmail.com>
2021-01-06 22:15:47 +0700

    penambahan script pada function GET untuk menarik data dari SIPD

--- app/Libraries/Client.php
+++ app/Libraries/Client.php
@@ -4,11 +4,22 @@ namespace App\Libraries;
 class Client{
 
     public function post($url,$param,$cookie){
-        
+
     }
 
     public function get($url,$cookie){
+        $opt=[
+            'http'=>[
+                'method'=>'GET',
+                'header'=>'Accept-language: en\r\n'.
+                          'Cookie: '.$cookie
+            ]
+        ];
+        $context=stream_context_create($opt);
+        $result=file_get_contents($url,false,$context);
 
+        $data=['header'=>$http_response_header,'content'=>$result];
+        return $data;
     }
 
     public function getCookie($data){
