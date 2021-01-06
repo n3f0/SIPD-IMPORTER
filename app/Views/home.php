a793851763adeb551f471808f9558c8a457c9800
n3f0 <nefo424@gmail.com>
2021-01-06 22:23:07 +0700

    penambahan style css

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,6 +2,27 @@
     <head>
         <title>.::SIPD Importer Tools::.</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                background-color: #17a2b8;
+                height: 100vh;
+                }
+                #login .container #login-row #login-column #login-box {
+                margin-top: 120px;
+                max-width: 600px;
+                height: 320px;
+                border: 1px solid #9C9C9C;
+                background-color: #EAEAEA;
+                }
+                #login .container #login-row #login-column #login-box #login-form {
+                padding: 20px;
+                }
+                #login .container #login-row #login-column #login-box #login-form #register-link {
+                margin-top: -85px;
+                }
+        </style>
     </head>
     <body>
         <div id="login">
