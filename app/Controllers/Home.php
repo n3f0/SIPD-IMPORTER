5f121289c447438d518126c421306158368101ec
n3f0 <nefo424@gmail.com>
2021-01-06 22:01:37 +0700

    Perbaikan tampilan awal

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends BaseController
 	}
 	public function index()
 	{
-		
+		return view('home');
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/home.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>.::SIPD Importer Tools::.</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
