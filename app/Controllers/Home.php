1864f3115587685dbf4ef579eaa8114b6642ccb1
n3f0 <nefo424@gmail.com>
2021-01-06 23:27:01 +0700

    test cookie dengan alamat sub domain daerah

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,15 +6,19 @@ class Home extends BaseController
 {
 	public function __construct(){
 		$this->client=new Client;
+		
 	}
 	public function index()
 	{
+		$session = \Config\Services::session($config);
+
 		return view('home');
 	}
 
 
 	public function test(){
-		$url="https://nusantaraprov.sipd.kemendagri.go.id/daerah";
+
+		$url="https://".SIPD.".sipd.kemendagri.go.id/daerah";
 
 		$data=$this->client->get($url);
 		
